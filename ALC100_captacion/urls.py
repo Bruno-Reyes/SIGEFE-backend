f42b5d8b77e0290fbad837dd036cc49a252a47ed
bruno-rg <garnelob@gmail.com>
2024-11-28 03:06:26 +0100

    Convocatorias + filtro

--- ALC100_captacion/urls.py
+++ ALC100_captacion/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from ALC100_captacion.views.views import ConvocatoriaViewSet
+from ALC100_captacion.views.views import ConvocatoriasActivas
 
 router = DefaultRouter()
 router.register(r'convocatorias', ConvocatoriaViewSet)
@@ -9,4 +10,5 @@ router.register(r'convocatorias', ConvocatoriaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('activas/', ConvocatoriasActivas.as_view(), name='convocatoria-list')
 ]
--- ALC100_captacion/views/views.py
+++ ALC100_captacion/views/views.py
@@ -1,8 +1,20 @@
 # views.py
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from ALC100_captacion.models.models import Convocatoria
 from ALC100_captacion.serializers import ConvocatoriaSerializer
+from django.utils import timezone
 
 class ConvocatoriaViewSet(viewsets.ModelViewSet):
     queryset = Convocatoria.objects.all()
     serializer_class = ConvocatoriaSerializer
+
+# Endpoint para obtener la lista de convocatorias activas
+# GET /api/convocatorias/activas/
+class ConvocatoriasActivas(APIView):
+    def get(self, request):
+        fecha_actual = timezone.now().date()
+        convocatorias = Convocatoria.objects.filter(fecha_limite_registro__gte=fecha_actual)
+        serializer = ConvocatoriaSerializer(convocatorias, many=True)
+        return Response(serializer.data)
