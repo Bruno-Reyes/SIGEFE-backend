01b50cdfed2493509525658cb7d61dfea66c6dfe
Brenda-pb <brenda.pb.rs@gmail.com>
2024-12-19 13:26:45 -0600

    agregar crear_pagos
    Se crearon los comentarios en poblar_db.py para importar crear_pagos

--- utils/crear_pagos.py
+++ utils/crear_pagos.py
@@ -39,4 +39,4 @@ def crear_pagos():
     
 
 
-crear_pagos()
+
--- utils/poblar_db.py
+++ utils/poblar_db.py
@@ -1,7 +1,9 @@
 from utils.crear_usuarios import crear_usuarios
 from utils.crear_convocatorias import crear_convocatorias
 from utils.crear_candidatos import crear_candidatos
+#from utils.crear_pagos import crear_pagos
 
 crear_usuarios()
 crear_convocatorias()
+#crear_pagos()
 #crear_candidatos(500)
