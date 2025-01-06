c6a4d17e35c14b1af39bd39dafbb742bd9198bc7
Lomosaurio123 <mhernandezl1605@alumno.ipn.mx>
2025-01-05 23:56:34 -0600

    asignar equipo a centro

--- ALC600_logistica/serializer.py
+++ ALC600_logistica/serializer.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from ALC600_logistica.models.models import EquipoDisponible
+from ALC600_logistica.models.models import EquipoDisponible, AsignacionMaterial
 
 class EquipoDisponibleSerializer(serializers.ModelSerializer):
     class Meta:
         model = EquipoDisponible
         fields = '__all__'
+
+class AsignacionMaterialSerializer(serializers.ModelSerializer):
+    nombre_equipo = serializers.CharField(source='equipo.nombre_equipo', read_only=True)
+    categoria = serializers.CharField(source='equipo.categoria', read_only=True)
+    clave_centro_trabajo = serializers.CharField(source='centro.clave_centro_trabajo', read_only=True)
+    estado = serializers.CharField(source='centro.estado', read_only=True)
+    municipio = serializers.CharField(source='centro.municipio', read_only=True)
+    cantidad = serializers.IntegerField(source='cantidad_asignada', read_only=True)
+
+    class Meta:
+        model = AsignacionMaterial
+        fields = [
+            'id',
+            'cantidad',
+            'nombre_equipo',
+            'categoria',
+            'clave_centro_trabajo',
+            'estado',
+            'municipio',
+        ]
--- ALC600_logistica/views/views.py
+++ ALC600_logistica/views/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ALC600_logistica.models.models import EquipoDisponible
-from ALC600_logistica.serializer import EquipoDisponibleSerializer
+from ALC600_logistica.models.models import EquipoDisponible, AsignacionMaterial
+from ALC600_logistica.serializer import AsignacionMaterialSerializer, EquipoDisponibleSerializer
 from rest_framework.decorators import permission_classes
+from ALC200_asignacion.models.models import CentroComunitario
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
@@ -31,3 +32,60 @@ class EquipoDisponibleCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+    
+
+@permission_classes([AllowAny])
+class CrearAsignacionView(APIView):
+    def post(self, request):
+
+        try:
+
+            # Obtener equipo y centro comunitario
+            equipo = EquipoDisponible.objects.get(id=request.data.get("equipo_id"))
+            centro = CentroComunitario.objects.get(id=request.data.get("centro_id"))
+
+            # Validar cantidad
+            cantidad = int(request.data.get("cantidad_asignada"))
+            if cantidad <= 0 or cantidad > equipo.cantidad_disponible:
+                return Response(
+                    {"error": "La cantidad no es válida o supera la cantidad disponible."},
+                )
+
+            # Crear la asignación
+            asignacion = AsignacionMaterial.objects.create(
+                equipo=equipo,
+                centro_id=centro.id,
+                cantidad_asignada=cantidad,
+            )
+
+            # Reducir la cantidad disponible del equipo
+            equipo.cantidad_disponible -= cantidad
+            equipo.save()
+
+            # Serializar la asignación
+            serializer = AsignacionMaterialSerializer(asignacion)
+
+            return Response(serializer.data)
+
+        except EquipoDisponible.DoesNotExist:
+            return Response({"error": f"El equipo seleccionado no existe."})
+        except CentroComunitario.DoesNotExist:
+            return Response({"error": "El centro seleccionado no existe."})
+        except Exception as e:
+            return Response({"error": str(e)})
+        
+        
+@permission_classes([AllowAny])
+class AsignacionListView(APIView):
+    def get(self, request):
+        try:
+            asignaciones = AsignacionMaterial.objects.all()
+            if not asignaciones.exists():
+                return Response({"message": "No hay asignaciones registradas."}, status=200)
+
+            serializer = AsignacionMaterialSerializer(asignaciones, many=True)
+            return Response(serializer.data, status=200)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            return Response({"error": f"Error interno: {str(e)}"}, status=500)
