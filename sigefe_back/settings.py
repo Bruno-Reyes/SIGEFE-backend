56a482b6df0ba76f5da05b9572d264d0329c4aec
Bruno-Reyes <garnelob@gmail.com>
2025-01-06 04:21:09 -0600

    Base de datos en VM configurada

--- sigefe_back/settings.py
+++ sigefe_back/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,8 +122,12 @@ WSGI_APPLICATION = 'sigefe_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('MYSQL_NAME_DATABASE'),
+        'USER': config('MYSQL_USERNAME'),
+        'PASSWORD': config('MYSQL_PASSWORD'),
+        'HOST': config('MYSQL_HOST'),
+        'PORT': config('MYSQL_PORT'),
     }
 }
 
